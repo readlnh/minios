8f2aaae9a1edbd252258e94e376d60215b85b0fb
readlnh <readlnh@163.com>
2017-05-16 19:08:16 +0800

    清屏

--- init/entry.c
+++ init/entry.c
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-/*
- * =====================================================================================
- *
- *       Filename:  entry.c
- *
- *    Description:  hurlex 内核的入口函数
- *
- *        Version:  1.0
- *        Created:  2013年10月31日 13时31分03秒
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
- *        Company:  Class 1107 of Computer Science and Technology
- *
- * =====================================================================================
- */
-
 #include "types.h"
 #include "console.h"
 
@@ -25,22 +6,5 @@ int kern_entry()
 	console_clear();
 
 	return 0;
-=======
-#include "int32_test.h"
-int kern_entry()
-{
-		
-	regs16_t regs;	
-	regs.ax = 0x0013;
-	int32(0x10, &regs);
-	//unsigned int* buffer = (unsigned int*) 0xA0000;
-	int i;
-	char *p;	
-	for(i = 0xa0000; i < 0xaffff; i++) {
-		p = i;
-		*p = 15;	
-	}   
-	return 0;	
->>>>>>> 0458cc73481b3c9d5253296281282deacf1aead9
 }
 
